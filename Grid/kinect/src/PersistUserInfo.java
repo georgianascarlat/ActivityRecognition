19bc5e8f36f6648381d85ea53c96fc61cef1e6db
gia <gia@yuki>
2013-04-10 23:12:16 +0300

    remove stuff + great cell detection

--- Grid/kinect/src/PersistUserInfo.java
+++ Grid/kinect/src/PersistUserInfo.java
@@ -19,23 +19,7 @@ import processing.core.*;
 public class PersistUserInfo extends PApplet {
 
 	private static final int MAX_ITERATIONS = 500;
-	final static int CALIB_START = 0;
-	final static int CALIB_NULLPOINT = 1;
-	final static int CALIB_X_POINT = 2;
-	final static int CALIB_Z_POINT = 3;
-	final static int CALIB_DONE = 4;
-
-	boolean screenFlag = true;
-	int calibMode = CALIB_START;
-
-	PVector nullPoint3d = new PVector();
-	PVector xDirPoint3d = new PVector();
-	PVector zDirPoint3d = new PVector();
-	PVector tempVec1 = new PVector();
-	PVector tempVec2 = new PVector();
-	PVector tempVec3 = new PVector();
-
-	PMatrix3D userCoordsysMat = new PMatrix3D();
+	
 
 	private static final int SNAPSHOT_INIT_DELAY = 1600;
 	private static final int SNAPSHOT_DELAY = 600;
@@ -133,68 +117,7 @@ public class PersistUserInfo extends PApplet {
 		
 		image(context.rgbImage(), 0, 0);
 
-		// Put text to help you choose coordSys
-		// draw text background
-		pushStyle();
-		noStroke();
-		fill(0, 200, 0, 100);
-		rect(0, 0, width, 40);
-		popStyle();
-
-		// write text to help the user choose the coordSys
-		switch (calibMode) {
-		case CALIB_START:
-			text("To start the calibration press SPACE!", 5, 30);
-			break;
-		case CALIB_NULLPOINT:
-			text("Set the nullpoint with the left mousebutton", 5, 30);
-			break;
-		case CALIB_X_POINT:
-			text("Set the x-axis with the left mousebutton", 5, 30);
-			break;
-		case CALIB_Z_POINT:
-			text("Set the z-axis with the left mousebutton", 5, 30);
-			break;
-		case CALIB_DONE:
-			text("New nullpoint is defined!", 5, 30);
-			break;
-		}
-
-		// draw
-		drawCalibPoint();
-
-		// draw the user defined coordinate system
-		// with the size of 500mm
-		if (context.hasUserCoordsys()) {
-			PVector temp = new PVector();
-			PVector nullPoint = new PVector();
-
-			pushStyle();
-
-			strokeWeight(3);
-			noFill();
-
-			context.convertRealWorldToProjective(new PVector(0, 0, 0), tempVec1);
-			stroke(255, 255, 255, 150);
-			ellipse(tempVec1.x, tempVec1.y, 10, 10);
-
-			context.convertRealWorldToProjective(new PVector(500, 0, 0),
-					tempVec2);
-			stroke(255, 0, 0, 150);
-			line(tempVec1.x, tempVec1.y, tempVec2.x, tempVec2.y);
-
-			context.convertRealWorldToProjective(new PVector(0, 500, 0),
-					tempVec2);
-			stroke(0, 255, 0, 150);
-			line(tempVec1.x, tempVec1.y, tempVec2.x, tempVec2.y);
-
-			context.convertRealWorldToProjective(new PVector(0, 0, 500),
-					tempVec2);
-			stroke(0, 0, 255, 150);
-			line(tempVec1.x, tempVec1.y, tempVec2.x, tempVec2.y);
-
-			popStyle();
-		}
+		
 
 		// draw the skeleton if it's available
 		int i;
@@ -235,60 +158,7 @@ public class PersistUserInfo extends PApplet {
 
 	}
 
-	// draw the points chosen for the coordSys so far
-	void drawCalibPoint() {
-		pushStyle();
-
-		strokeWeight(3);
-		noFill();
-
-		switch (calibMode) {
-		case CALIB_START:
-			break;
-		case CALIB_NULLPOINT:
-			context.convertRealWorldToProjective(nullPoint3d, tempVec1);
-
-			stroke(255, 255, 255, 150);
-			ellipse(tempVec1.x, tempVec1.y, 10, 10);
-			break;
-		case CALIB_X_POINT:
-			// draw the null point
-			context.convertRealWorldToProjective(nullPoint3d, tempVec1);
-			context.convertRealWorldToProjective(xDirPoint3d, tempVec2);
-
-			stroke(255, 255, 255, 150);
-			ellipse(tempVec1.x, tempVec1.y, 10, 10);
-
-			stroke(255, 0, 0, 150);
-			ellipse(tempVec2.x, tempVec2.y, 10, 10);
-			line(tempVec1.x, tempVec1.y, tempVec2.x, tempVec2.y);
-
-			break;
-		case CALIB_Z_POINT:
-
-			context.convertRealWorldToProjective(nullPoint3d, tempVec1);
-			context.convertRealWorldToProjective(xDirPoint3d, tempVec2);
-			context.convertRealWorldToProjective(zDirPoint3d, tempVec3);
-
-			stroke(255, 255, 255, 150);
-			ellipse(tempVec1.x, tempVec1.y, 10, 10);
-
-			stroke(255, 0, 0, 150);
-			ellipse(tempVec2.x, tempVec2.y, 10, 10);
-			line(tempVec1.x, tempVec1.y, tempVec2.x, tempVec2.y);
-
-			stroke(0, 0, 255, 150);
-			ellipse(tempVec3.x, tempVec3.y, 10, 10);
-			line(tempVec1.x, tempVec1.y, tempVec3.x, tempVec3.y);
-
-			break;
-		case CALIB_DONE:
-
-			break;
-		}
-
-		popStyle();
-	}
+	
 
 	// display all floor points
 	private void showFloorPoints(List<Point3f> planPoints, PVector proj) {
@@ -340,43 +210,39 @@ public class PersistUserInfo extends PApplet {
 	
 	public void fitFloorMargins(){
 		
-		PVector p1 = new PVector(), p2 = new PVector(), p3 = new PVector(), p4 = new PVector();
+		PVector point = new PVector(), p2 = new PVector(), p3 = new PVector(), p4 = new PVector();
 		List<Point3f> floorM;
 		List<PVector> projectedPoints = new LinkedList<PVector>();
 		int k = 0;
+		boolean allGood;
 
-		while(true){
+		while(k < MAX_ITERATIONS){
 			
-			k++;
+			
 			projectedPoints = new LinkedList<PVector>();
 			
 			floorM = floor.getMargins();
-			p1 = new PVector(floorM.get(0).x, floorM.get(0).y, floorM.get(0).z);
-			context.convertRealWorldToProjective(p1, p1);
-			projectedPoints.add(p1);
 			
-			p2 = new PVector(floorM.get(1).x, floorM.get(1).y, floorM.get(1).z);
-			context.convertRealWorldToProjective(p2, p2);
-			projectedPoints.add(p2);
+			for(int i=0;i<floorM.size();i++){
+				point = new PVector(floorM.get(i).x, floorM.get(i).y, floorM.get(i).z);
+				context.convertRealWorldToProjective(point, point);
+				projectedPoints.add(point);
+			}
 			
-			p3 = new PVector(floorM.get(2).x, floorM.get(2).y, floorM.get(2).z);
-			context.convertRealWorldToProjective(p3, p3);
-			projectedPoints.add(p3);
+			allGood = true;
+			for(PVector pV:projectedPoints){
+				if(!insideImage(pV))
+					allGood = false;
+			}
 			
-			p4 = new PVector(floorM.get(3).x, floorM.get(3).y, floorM.get(3).z);
-			context.convertRealWorldToProjective(p4, p4);
-			projectedPoints.add(p4);
-			
-			
-			if(insideImage(p1) && insideImage(p2) && insideImage(p3) && insideImage(p4))
+			if(allGood)
 				break;
 			
 			
 			floor.adjustMargins(projectedPoints,100,context.depthWidth(),0, context.depthHeight());
 			
 			
-			if(k == MAX_ITERATIONS)
-				break;
+			k++;
 			
 			
 		}
@@ -549,94 +415,8 @@ public class PersistUserInfo extends PApplet {
 	// -----------------------------------------------------------------
 	// SimpleOpenNI events
 	
-	public void keyPressed() {
-		switch (key) {
-		case '1':
-			screenFlag = !screenFlag;
-			break;
-		case ' ':
-			calibMode++;
-			if (calibMode > CALIB_DONE) {
-				calibMode = CALIB_START;
-				context.resetUserCoordsys();
-			} else if (calibMode == CALIB_DONE) {
-				// set the calibration
-				context.setUserCoordsys(nullPoint3d.x, nullPoint3d.y,
-						nullPoint3d.z, xDirPoint3d.x, xDirPoint3d.y,
-						xDirPoint3d.z, zDirPoint3d.x, zDirPoint3d.y,
-						zDirPoint3d.z);
 
-				println("Set the user define coordinatesystem");
-				println("nullPoint3d: " + nullPoint3d);
-				println("xDirPoint3d: " + xDirPoint3d);
-				println("zDirPoint3d: " + zDirPoint3d);
 
-				/*
-				 * // test context.getUserCoordsysTransMat(userCoordsysMat);
-				 * PVector temp = new PVector();
-				 * 
-				 * userCoordsysMat.mult(new PVector(0, 0, 0), temp);
-				 * println("PVector(0,0,0): " + temp);
-				 * 
-				 * userCoordsysMat.mult(new PVector(500, 0, 0), temp);
-				 * println("PVector(500,0,0): " + temp);
-				 * 
-				 * userCoordsysMat.mult(new PVector(0, 500, 0), temp);
-				 * println("PVector(0,500,0): " + temp);
-				 * 
-				 * userCoordsysMat.mult(new PVector(0, 0, 500), temp);
-				 * println("PVector(0,0,500): " + temp);
-				 */
-			}
-
-			break;
-		}
-	}
-
-	public void mousePressed() {
-		if (mouseButton == LEFT) {
-			PVector[] realWorldMap = context.depthMapRealWorld();
-			int index = mouseX + mouseY * context.depthWidth();
-
-			switch (calibMode) {
-			case CALIB_NULLPOINT:
-				nullPoint3d.set(realWorldMap[index]);
-				break;
-			case CALIB_X_POINT:
-				xDirPoint3d.set(realWorldMap[index]);
-				break;
-			case CALIB_Z_POINT:
-				zDirPoint3d.set(realWorldMap[index]);
-				break;
-			}
-		} else {
-			PVector[] realWorldMap = context.depthMapRealWorld();
-			int index = mouseX + mouseY * context.depthWidth();
-
-			println("Point3d: " + realWorldMap[index].x + ","
-					+ realWorldMap[index].y + "," + realWorldMap[index].z);
-		}
-	}
-
-	public void mouseDragged() {
-		if (mouseButton == LEFT) {
-			PVector[] realWorldMap = context.depthMapRealWorld();
-			int index = mouseX + mouseY * context.depthWidth();
-
-			switch (calibMode) {
-			case CALIB_NULLPOINT:
-				nullPoint3d.set(realWorldMap[index]);
-				break;
-			case CALIB_X_POINT:
-				xDirPoint3d.set(realWorldMap[index]);
-				break;
-			case CALIB_Z_POINT:
-				zDirPoint3d.set(realWorldMap[index]);
-				break;
-			}
-		}
-
-	}
 	public void onNewUser(int userId) {
 		println("onNewUser - userId: " + userId);
 		println("  start pose detection");
